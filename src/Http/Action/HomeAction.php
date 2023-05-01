f61fa580a935882e1053e84f62fa289b5ea43af8
Alexey Shmelev <alexsey_89@bk.ru>
2023-05-01 20:58:50 +0300

    update configs

--- src/Http/Action/HomeAction.php
+++ src/Http/Action/HomeAction.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace Service\Http\Action;
 
+use JsonException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Service\Slim\Response\HtmlResponse;
+use Service\Slim\Response\JsonResponse;
+use stdClass;
 
 final class HomeAction implements RequestHandlerInterface
 {
+    /**
+     * @throws JsonException
+     */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        return new HtmlResponse('html');
+        return new JsonResponse(new stdClass());
     }
 }
