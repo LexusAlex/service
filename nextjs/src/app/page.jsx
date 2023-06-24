93aa6b0fb74f44bff119dce6dcb730b69a47cebe
Alexey Shmelev <alexsey_89@bk.ru>
2023-06-25 00:37:01 +0300

    install clear next js

--- nextjs/src/app/page.jsx
+++ nextjs/src/app/page.jsx
@@ -2,49 +2,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="container text-center">
-        <div className="row align-items-start">
-          <div className="col">
-            One of three columns
-          </div>
-          <div className="col">
-            One of three columns
-          </div>
-          <div className="col">
-            One of three columns
-          </div>
-          <h1>h1. Bootstrap heading</h1>
-          <h2>h2. Bootstrap heading</h2>
-          <h3>h3. Bootstrap heading</h3>
-          <h4>h4. Bootstrap heading</h4>
-          <h5>h5. Bootstrap heading</h5>
-          <h6>h6. Bootstrap heading</h6>
-          <div id="liveAlertPlaceholder"></div>
-          <button type="button" className="btn btn-primary" id="liveAlertBtn">Show live alert</button>
-          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Launch demo modal
-          </button>
-
-          <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                  ...
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="button" className="btn btn-primary">Save changes</button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
+      page
     </main>
   )
 }
